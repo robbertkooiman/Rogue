3d37d7c0eef60a6f003ce44ec6f09f586e54a929
Unknown <jimi.verhoeven@gmail.com>
2018-07-26 21:19:30 +0200

    Start of enemy general class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Actor {
+
+	private Rigidbody2D body;
+	
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	void GetHit(float amount) {
+		base.damage(amount);
+		float hitDirection = 100f; // check from where the player hit the emeny
+		body.AddForce(new Vector2(hitDirection, 10f)); // make the enmey move back
+	}
+
+	void Attack() {
+		// do attack at player
+	}
+
+	override protected void Die(){
+		base.Die();
+		// remove enemy/show corpse
+		// grant player coins/xp/whatever
+	}
+}
